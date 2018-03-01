386e9b255351a6ef15c5bbac41478aaa4c70d6c7
Jamie Stewart <jstewart39@nait.ca>
2018-03-01 15:24:28 -0700

    Created ChangeSurveyWord.aspx Webpage

---
+++ FSOSS Project/FSOSS Website/Pages/AdministratorPages/MasterAdministratorPages/ChangeSurveyWord.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_AdministratorPages_MasterAdministratorPages_ChangeSurveyWord : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SearchWordButton_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void AddWordButton_Click(object sender, EventArgs e)
+    {
+
+    }
+}
