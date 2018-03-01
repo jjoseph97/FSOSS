61a8f35cc4fa6daefc7121f001b892ff90dd74d4
James Joseph <james.joseph@hotmail.ca>
2018-03-01 16:09:57 -0700

    Fixed - PotentialSurveyWordController

--- FSOSS Project/FSOSS.System/BLL/PotentialSurveyWordController.cs
+++ FSOSS Project/FSOSS.System/BLL/PotentialSurveyWordController.cs
@@ -13,143 +13,143 @@ using FSOSS.System.Data.POCOs;
 
 namespace FSOSS.System.BLL
 {
-    [DataObject]
-    public class PotentialSurveyWordController
-    {
-        //Add new Potential Word
-        [DataObjectMethod(DataObjectMethodType.Insert, false)]
-        public string AddWord(string newWord)
-        {
-            // transaction start
-            using (var context = new FSOSSContext())
-            {
-                string message = "";
-                try
-                {
-                    PotentialSurveyWord potentialSurveyWord = new PotentialSurveyWord();
-                    potentialSurveyWord.user_id = 1;
-                    potentialSurveyWord.survey_access_word = newWord;
-                    context.PotentialSurveyWords.Add(potentialSurveyWord);
-                    context.SaveChanges();
-                    message = "Successfully Added a New Word!";
-                }
-                catch (Exception e)
-                {
-                    message = "Oops, something went wrong. Check " + e.Message;
-                }
-                return message;
+    //[DataObject]
+    //public class PotentialSurveyWordController
+    //{
+    //    //Add new Potential Word
+    //    [DataObjectMethod(DataObjectMethodType.Insert, false)]
+    //    public string AddWord(string newWord)
+    //    {
+    //        // transaction start
+    //        using (var context = new FSOSSContext())
+    //        {
+    //            string message = "";
+    //            try
+    //            {
+    //                PotentialSurveyWord potentialSurveyWord = new PotentialSurveyWord();
+    //                potentialSurveyWord.user_id = 1;
+    //                potentialSurveyWord.survey_access_word = newWord;
+    //                context.PotentialSurveyWords.Add(potentialSurveyWord);
+    //                context.SaveChanges();
+    //                message = "Successfully Added a New Word!";
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                message = "Oops, something went wrong. Check " + e.Message;
+    //            }
+    //            return message;
 
-            } //end of transaction
-        } // end of method AddWord
+    //        } //end of transaction
+    //    } // end of method AddWord
 
-        [DataObjectMethod(DataObjectMethodType.Update, false)]
-        public string UpdateWord(PotentialSurveyWord surveyWord)
-        {
-            using (var context = new FSOSSContext())
-            {
-                string message = "";
-                try
-                {
-                    PotentialSurveyWord potentialSurveyWord = (from x in context.PotentialSurveyWords
-                                                               where x.survey_word_id == surveyWord.survey_word_id
-                                                               select x).FirstOrDefault();
+    //    [DataObjectMethod(DataObjectMethodType.Update, false)]
+    //    public string UpdateWord(PotentialSurveyWord surveyWord)
+    //    {
+    //        using (var context = new FSOSSContext())
+    //        {
+    //            string message = "";
+    //            try
+    //            {
+    //                PotentialSurveyWord potentialSurveyWord = (from x in context.PotentialSurveyWords
+    //                                                           where x.survey_word_id == surveyWord.survey_word_id
+    //                                                           select x).FirstOrDefault();
 
-                    //Not sure if update works with default constraint. Subject for testing
-                    potentialSurveyWord.survey_access_word = surveyWord.survey_access_word;
-                    context.SaveChanges();
+    //                //Not sure if update works with default constraint. Subject for testing
+    //                potentialSurveyWord.survey_access_word = surveyWord.survey_access_word;
+    //                context.SaveChanges();
 
-                    message = "Successfully Updated the Word!";
-                }
-                catch (Exception e)
-                {
-                    message = "Oops, something went wrong. Check " + e.Message;
-                }
-                return message;
-            }
-        }
+    //                message = "Successfully Updated the Word!";
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                message = "Oops, something went wrong. Check " + e.Message;
+    //            }
+    //            return message;
+    //        }
+    //    }
 
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<PotentialSurveyWordPOCO> GetSurveyWord(string surveyWord)
-        {
-            using (var context = new FSOSSContext())
-            {
+    //    [DataObjectMethod(DataObjectMethodType.Select, false)]
+    //    public List<PotentialSurveyWordPOCO> GetSurveyWord(string surveyWord)
+    //    {
+    //        using (var context = new FSOSSContext())
+    //        {
 
-                try
-                {
+    //            try
+    //            {
 
-                    var potentialSurveyWordList = from x in context.PotentialSurveyWords
-                                                  where x.survey_access_word.Contains("/" + surveyWord + "/")
-                                                  select new PotentialSurveyWordPOCO()
-                                                  {
-                                                      surveyWordID = x.survey_word_id,
-                                                      surveyWord = x.survey_access_word
-                                                  };
+    //                var potentialSurveyWordList = from x in context.PotentialSurveyWords
+    //                                              where x.survey_access_word.Contains("/" + surveyWord + "/")
+    //                                              select new PotentialSurveyWordPOCO()
+    //                                              {
+    //                                                  surveyWordID = x.survey_word_id,
+    //                                                  surveyWord = x.survey_access_word
+    //                                              };
 
-                    return potentialSurveyWordList.ToList();
-
-
-                }
-                catch (Exception e)
-                {
-                    throw new Exception("Something went wrong. See " + e.Message);
-                }
-
-            }
-        }
-
-        [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<PotentialSurveyWordPOCO> GetAllSurveyWord()
-        {
-            using (var context = new FSOSSContext())
-            {
-
-                try
-                {
-
-                    var potentialSurveyWordList = from x in context.PotentialSurveyWords
-                                                  select new PotentialSurveyWordPOCO()
-                                                  {
-                                                      surveyWordID = x.survey_word_id,
-                                                      surveyWord = x.survey_access_word
-                                                  };
-
-                    return potentialSurveyWordList.ToList();
+    //                return potentialSurveyWordList.ToList();
 
 
-                }
-                catch (Exception e)
-                {
-                    throw new Exception("Something went wrong. See " + e.Message);
-                }
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                throw new Exception("Something went wrong. See " + e.Message);
+    //            }
 
-            }
-        }
+    //        }
+    //    }
 
-        [DataObjectMethod(DataObjectMethodType.Delete, false)]
-        public string DeleteWord(int surveyWordID)
-        {
-            using (var context = new FSOSSContext())
-            {
-                string message = "";
-                try
-                {
+    //    [DataObjectMethod(DataObjectMethodType.Select, false)]
+    //    public List<PotentialSurveyWordPOCO> GetAllSurveyWord()
+    //    {
+    //        using (var context = new FSOSSContext())
+    //        {
 
-                    PotentialSurveyWord potentialSurveyWord = context.PotentialSurveyWords.Find(surveyWordID);
-                    context.PotentialSurveyWords.Remove(potentialSurveyWord);
-                    context.SaveChanges();
+    //            try
+    //            {
+
+    //                var potentialSurveyWordList = from x in context.PotentialSurveyWords
+    //                                              select new PotentialSurveyWordPOCO()
+    //                                              {
+    //                                                  surveyWordID = x.survey_word_id,
+    //                                                  surveyWord = x.survey_access_word
+    //                                              };
+
+    //                return potentialSurveyWordList.ToList();
 
 
-                    message = "Succesfully Remove Survey Word";
-                    return message;
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                throw new Exception("Something went wrong. See " + e.Message);
+    //            }
+
+    //        }
+    //    }
+
+    //    [DataObjectMethod(DataObjectMethodType.Delete, false)]
+    //    public string DeleteWord(int surveyWordID)
+    //    {
+    //        using (var context = new FSOSSContext())
+    //        {
+    //            string message = "";
+    //            try
+    //            {
+
+    //                PotentialSurveyWord potentialSurveyWord = context.PotentialSurveyWords.Find(surveyWordID);
+    //                context.PotentialSurveyWords.Remove(potentialSurveyWord);
+    //                context.SaveChanges();
 
 
-                }
-                catch (Exception e)
-                {
-                    throw new Exception("Something went wrong. See " + e.Message);
-                }
+    //                message = "Succesfully Remove Survey Word";
+    //                return message;
 
-            }
-        }
-    }
+
+    //            }
+    //            catch (Exception e)
+    //            {
+    //                throw new Exception("Something went wrong. See " + e.Message);
+    //            }
+
+    //        }
+    //    }
+    //}
 }
