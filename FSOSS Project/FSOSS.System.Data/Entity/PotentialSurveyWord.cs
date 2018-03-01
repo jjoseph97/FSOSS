ef3936b536e62b3dbf206a91212e9a02ecf02bc0
James Joseph <james.joseph@hotmail.ca>
2018-03-01 15:40:43 -0700

    Added Entity - PotentialSurveyWord

---
+++ FSOSS Project/FSOSS.System.Data/Entity/PotentialSurveyWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using FSOSS.System;
+using System.Data;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FSOSS.System.Data.Entity
+{
+    [Table("potential_survey_word", Schema = "public")]
+    public class PotentialSurveyWord
+    {
+        public int survey_word_id { get; set; }
+        public int user_id { get; set; }
+        public string survey_access_word { get; set; }
+        public DateTime date_modified { get; set; }
+    }
+}
