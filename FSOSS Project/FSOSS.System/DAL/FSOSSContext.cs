67253ee9017c622fa2bce579acd995302b37cd99
bHS <bHS@bHS.com>
2018-03-04 10:24:25 -0700

    Setup DAL for new Entities

--- FSOSS Project/FSOSS.System/DAL/FSOSSContext.cs
+++ FSOSS Project/FSOSS.System/DAL/FSOSSContext.cs
@@ -18,5 +18,22 @@ namespace FSOSS.System.DAL
 
         public virtual DbSet<ParticipantType> ParticipantTypes { get; set; }
         public virtual DbSet<PotentialSurveyWord> PotentialSurveyWords { get; set; }
+        public virtual DbSet<AdministratorAccount> AdministratorAccounts { get; set; }
+        public virtual DbSet<AdministratorRole> AdministratorRoles { get; set; }
+        public virtual DbSet<AgeRange> AgeRanges { get; set; }
+        public virtual DbSet<Gender> Genders { get; set; }
+        public virtual DbSet<Meal> Meals { get; set; }
+        public virtual DbSet<ParticipantResponse> ParticipantResponses { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<QuestionFormat> QuestionFormats { get; set; }
+        public virtual DbSet<QuestionSelection> QuestionSelections { get; set; }
+        public virtual DbSet<QuestionTopic> QuestionTopics { get; set; }
+        public virtual DbSet<SecurityRole> SecurityRoles { get; set; }
+        public virtual DbSet<Site> Sites { get; set; }
+        public virtual DbSet<SubmittedSurvey> SubmittedSurveys { get; set; }
+        public virtual DbSet<SurveyQuestion> SurveyQuestions { get; set; }
+        public virtual DbSet<SurveyVersion> SurveyVersions { get; set; }
+        public virtual DbSet<SurveyWord> SurveyWords { get; set; }
+        public virtual DbSet<Unit> Units { get; set; }
     }
 }
