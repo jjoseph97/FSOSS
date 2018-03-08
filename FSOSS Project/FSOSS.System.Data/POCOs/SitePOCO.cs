c520b8db9c042a3bf6e9338bd115403d811f2d6c
chris andersen <Candersen10@studentmail.nait.ca>
2018-03-08 09:34:59 -0700

    SitePOCO Created
    ""

---
+++ FSOSS Project/FSOSS.System.Data/POCOs/SitePOCO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSOSS.System.Data.POCOs
+{
+    public class SitePOCO
+    {
+
+        public int siteID { get; set; }
+        public string siteName { get; set; }
+
+    }
+}
