17bb69ee06dbe4b9225bd37744ee602cfc85aea5
bHS <bHS@bHS.com>
2018-03-08 09:21:25 -0700

    Units Crud EMPTY PagesAndControllerClass

---
+++ FSOSS Project/FSOSS Website/Pages/AdministratorPages/MasterAdministratorPages/UnitsCrud.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="UnitsCrud.aspx.cs" Inherits="Pages_AdministratorPages_MasterAdministratorPages_UnitsCrud" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+
+</asp:Content>
+
---
+++ FSOSS Project/FSOSS.System/BLL/UnitsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using System.ComponentModel;
+using FSOSS.System.Data.Entity;
+using FSOSS.System.DAL;
+#endregion
+namespace FSOSS.System.BLL
+{
+    public class UnitsController
+    {
+
+    }
+}
