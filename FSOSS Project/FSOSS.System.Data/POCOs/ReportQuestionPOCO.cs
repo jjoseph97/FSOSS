5a83a4938d011b26868ff1598aa974dd74fa309b
Renzelle Frank Rodrigueza <bHS@gmail.com>
2018-03-09 10:10:34 -0700

    Pushed Report Controller
    Update Report Controller "Potential" Solution

---
+++ FSOSS Project/FSOSS.System.Data/POCOs/ReportQuestionPOCO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSOSS.System.Data.POCOs
+{
+    public class ReportQuestionPOCO
+    {
+        public int question_id { get; set; }
+        public string question_text { get; set; }
+    }
+}
