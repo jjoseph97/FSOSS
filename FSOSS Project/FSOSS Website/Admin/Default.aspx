b5696d571d44d4741083ca7fcca93dcf8e0af6d5
Rameses Resuena <rdresuena@live.com>
2018-03-11 19:06:04 -0600

    Updated admin home page
    Changed look of the Administrator home page. When pending contact request is greater than 0; style will change to capture users attention.

--- FSOSS Project/FSOSS Website/Admin/Default.aspx
+++ FSOSS Project/FSOSS Website/Admin/Default.aspx
@@ -1,36 +1,43 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Pages_AdministratorPages_MainPage" %>
 
 <asp:Content ID="AdminMainPage" ContentPlaceHolderID="MainContent" runat="Server">
-    <div class="form-group row">
-        <asp:Label ID="HospitalLabel" runat="server" AssociatedControlID="HospitalDDL" CssClass="col-sm-2 col-md-2 col-form-label" Text="Hospital: " />
-        <div class="col-sm-10 col-md-4">
-            <asp:DropDownList ID="HospitalDDL" runat="server" CssClass="form-control" AppendDataBoundItems="true" OnSelectedIndexChanged="HospitalDDL_SelectedIndexChanged" AutoPostBack="true" DataSourceID="SiteODS" DataTextField="siteName" DataValueField="siteID">
-                
-            </asp:DropDownList>
+    <div class="card">
+        <div class="col-sm-12 text-center mt-3">
+            <asp:Label ID="WelcomeMessage" runat="server" Text="Welcome, " CssClass="h4" />
         </div>
-    </div>
-    <div class="row">
+
+        <div class="form-inline mx-auto my-3">
+            <asp:Label ID="HospitalLabel" runat="server" AssociatedControlID="HospitalDDL" CssClass="col-sm-4" Text="Hospital: " />
+            <asp:DropDownList ID="HospitalDDL" runat="server" CssClass="form-control col-sm-8" OnSelectedIndexChanged="HospitalDDL_SelectedIndexChanged" AutoPostBack="true" DataSourceID="SiteODS" DataTextField="siteName" DataValueField="siteID" />
+        </div>
+
         <div class="col-sm-12 text-center">
-            <b>The word of the day is: </b>
-            <asp:Label ID="Placeholder" runat="server" />
+            <asp:Label Text="The word of the day is:" runat="server" Font-Bold="true" />
+            <asp:Label ID="WOTDLabel" runat="server" CssClass="h4" Font-Bold="true" />
         </div>
-    </div>
-    <div class="form-group row text-center">
-        <asp:Label ID="PendingRequestsLabel" runat="server" Text="Pending Contact Requests:   " CssClass="bg-dark text-white" />
-        <asp:Label ID="PendingRequestNumberLabel" runat="server" CssClass="bg-dark text-white" />
-        <asp:Button ID="ViewButton" runat="server" Text="View" CssClass="btn btn-primary" OnClick="ViewButton_Click" />
-    </div>
-    <div class="form-group row text-center">
-        <asp:Button ID="RecentSurveysButton" runat="server" Text="View Recent Surveys" CssClass="btn btn-primary" OnClick="RecentSurveysButton_Click" />
-    </div>
-    <div class="form-group row text-center">
-        <asp:Button ID="RecentReportsButton" runat="server" Text="View Recent Reports" CssClass="btn btn-primary" OnClick="RecentReportsButton_Click" />
+
+        <div id="PendingContactSection" runat="server" class="px-1 my-3 mx-auto">
+            <div class="form-inline mx-auto my-3">
+                <asp:Label ID="PendingRequestsLabel" runat="server" Text="Pending Contact Requests:" />
+                <asp:Label ID="PendingRequestNumberLabel" runat="server" />
+                <asp:Button ID="ViewButton" runat="server" Text="View" CssClass="btn btn-primary" OnClick="ViewButton_Click" />
+            </div>
+        </div>
+
+
+        <div class="form-group mx-auto">
+            <asp:Button ID="RecentSurveysButton" runat="server" Text="View Recent Surveys" CssClass="btn btn-primary" OnClick="RecentSurveysButton_Click" />
+        </div>
+
+        <div class="form-group mx-auto">
+            <asp:Button ID="RecentReportsButton" runat="server" Text="View Recent Reports" CssClass="btn btn-primary" OnClick="RecentReportsButton_Click" />
+        </div>
+
+
     </div>
 
-    <%-- Delete below --%>
-    <div class="row text-center">
-        <asp:Label ID="message" runat="server" />
-    </div>
+
+
     <asp:ObjectDataSource ID="SiteODS" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="GetSiteList" TypeName="FSOSS.System.BLL.SiteController"></asp:ObjectDataSource>
     <asp:ObjectDataSource ID="SiteContactCountODS" runat="server" OldValuesParameterFormatString="original_{0}" SelectMethod="GetContactRequestTotal" TypeName="FSOSS.System.BLL.SubmittedSurveyController">
         <SelectParameters>
--- FSOSS Project/FSOSS Website/Admin/Default.aspx.cs
+++ FSOSS Project/FSOSS Website/Admin/Default.aspx.cs
@@ -10,47 +10,47 @@ public partial class Pages_AdministratorPages_MainPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        message.Text = HttpContext.Current.Request.RawUrl.StartsWith("/Pages/AdministratorPages").ToString();
-
-        
-        if (Session["securityID"] == null || Session["securityID"].ToString() == "") // Redirect user to login if not logged in
+        if (Session["securityID"] == null) // Redirect user to login if not logged in
         {
             Response.Redirect("~/Admin/Login.aspx");
         }
-        else if (Session["securityID"].ToString() != "2") // Return HTTP Code 403 if security ID is not 2 (Master Administrator)
+        else if (Session["securityID"].ToString() != "2" && Session["securityID"].ToString() != "1") // Return HTTP Code 403
         {
             Context.Response.StatusCode = 403;
         }
-
-        // this will need to change
-        // TODO: Look up how to asynchronous pending requests
-        if (!Page.IsPostBack)
+        else
         {
-            Placeholder.Text = "";
+            if (!Page.IsPostBack)
+            {
+                WOTDLabel.Text += "abc"; // Replace with proper Word of the day
 
-            SubmittedSurveyController ssc = new SubmittedSurveyController();
-            HospitalDDL.DataBind();
-            HospitalDDL.SelectedValue = "1";
+                SubmittedSurveyController ssc = new SubmittedSurveyController();
+                HospitalDDL.DataBind();
+                HospitalDDL.SelectedValue = "1";
 
-            string value = HospitalDDL.SelectedValue;
-            if (value!=null) {
-                //PendingRequestNumberLabel.Text = value;
-                int siteID = Convert.ToInt32(value);
-                int contactCount = ssc.GetContactRequestTotal(siteID);
-                PendingRequestNumberLabel.Text = "&nbsp;"+contactCount.ToString()+ " &nbsp;";
+                string value = HospitalDDL.SelectedValue;
+                if (value != null)
+                {
+                    //PendingRequestNumberLabel.Text = value;
+                    int siteID = Convert.ToInt32(value);
+                    int contactCount = ssc.GetContactRequestTotal(siteID);
+
+                    if (contactCount > 0)
+                    {
+                        PendingRequestNumberLabel.Text = "&nbsp;" + contactCount.ToString() + " &nbsp;";
+                        PendingRequestNumberLabel.Font.Bold = true;
+                        PendingRequestNumberLabel.CssClass += " text-danger";
+                        PendingContactSection.Attributes["class"] += " card border-dark bg-warning";
+                    }
+                    else
+                    {
+                        PendingRequestNumberLabel.Text = "&nbsp; 0 &nbsp;";
+                        ViewButton.Visible = false;
+                    }
+                }
             }
-        }
-        
-        
-        // below is how to handle query strings from URL
-        // example: website.ca?bob=123
-        // ? starts query
-        // bob is identifier
-        // 123 is the value of bob
-        if (!String.IsNullOrEmpty(Request.QueryString["id"]))
-        {
-            string idtest = Convert.ToString(Request.QueryString["id"]);
-            //message.Text = "The ID passed in was: " + idtest;
+
+            WelcomeMessage.Text += Session["username"].ToString();
         }
     }
 
@@ -70,8 +70,6 @@ public partial class Pages_AdministratorPages_MainPage : System.Web.UI.Page
 
     protected void ViewButton_Click(object sender, EventArgs e)
     {
-        
-        message.Text = "You clicked the \"View\" Button";
         string value = HospitalDDL.SelectedValue;
         int siteID = Convert.ToInt32(value);
         string url = "~/Admin/ContactListPage.aspx?field=" + siteID.ToString();
@@ -81,27 +79,11 @@ public partial class Pages_AdministratorPages_MainPage : System.Web.UI.Page
 
     protected void RecentSurveysButton_Click(object sender, EventArgs e)
     {
-        message.Text = "You clicked the \"View Recent Surveys\" Button";
 
-        message.Text = Session["userID"].ToString();
     }
 
     protected void RecentReportsButton_Click(object sender, EventArgs e)
     {
-        message.Text = "You clicked the \"View Recent Reports\" Button";
-
-        AdministratorAccountController test = new AdministratorAccountController();
-        message.Text = test.GetSecurityID(1).ToString();
-        switch (Session["securityID"].ToString())
-        {
-            case "1":
-                message.Text = "Standard Admin";
-                break;
-            case "2":
-                message.Text = "Master Admin";
-                break;
-
-        }
 
     }
 }
