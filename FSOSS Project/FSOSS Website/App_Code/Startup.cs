367620de5a64b29ee4f881a5fc070639ab62e9de
Renzelle Frank Rodrigueza <rrodrigueza@nait.ca>
2018-03-15 22:20:34 -0600

    force add plugins

--- FSOSS Project/FSOSS Website/App_Code/Startup.cs
+++ FSOSS Project/FSOSS Website/App_Code/Startup.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Hangfire;
+using Hangfire.PostgreSql;
+using FSOSS.System.BLL;
 
 [assembly: OwinStartupAttribute(typeof(FSOSS_Website.Startup))]
 namespace FSOSS_Website
@@ -7,6 +10,13 @@ namespace FSOSS_Website
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
             ConfigureAuth(app);
+            app.UseHangfireServer(new BackgroundJobServerOptions(), new PostgreSqlStorage("User ID = postgres; Password = Password1; Host = localhost; Port = 5432; Database = FSOSSDatabase; Pooling = false;"));
+            JobStorage.Current = new PostgreSqlStorage("User ID = postgres; Password = Password1; Host = localhost; Port = 5432; Database = FSOSSDatabase; Pooling = false;");
+            app.UseHangfireDashboard();
+            SurveyWordController sysmgr = new SurveyWordController();
+            RecurringJob.AddOrUpdate(() => sysmgr.Execute(), Cron.Daily);
+            // Check if recurring works
+            //RecurringJob.AddOrUpdate(() => sysmgr.Sample(), Cron.Minutely);
         }
     }
 }
---
+++ FSOSS Project/FSOSS.System/BLL/SurveyWordController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSOSS.System.BLL
+{
+    public class SurveyWordController
+    {
+        public void Execute()
+        {
+
+        }
+    }
+}
