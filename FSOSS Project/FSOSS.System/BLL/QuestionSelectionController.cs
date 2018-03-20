df5c4626c1c1f14943ed5669ab7eac21e35185c6
James Joseph <james.joseph@hotmail.ca>
2018-03-20 11:05:09 -0600

    LINQ Queries re-added

--- FSOSS Project/FSOSS.System/BLL/QuestionSelectionController.cs
+++ FSOSS Project/FSOSS.System/BLL/QuestionSelectionController.cs
@@ -12,18 +12,138 @@ namespace FSOSS.System.BLL
     [DataObject]
     public class QuestionSelectionController
     {
+        //Question 1A
         [DataObjectMethod(DataObjectMethodType.Select, false)]
-        public List<ResponsePOCO> GetQuestionReponse(int question_id)
+        public List<ResponsePOCO> GetQuestion1AReponse()
         {
             using (var context = new FSOSSContext())
             {
                 var result = from x in context.QuestionSelections
-                             where x.question_id == question_id
-                             select new ResponsePOCO
-                             {
-                                 Text = x.question_selection_text,
-                                 Value = x.question_selection_value
-                             };
+                                where x.question_id == 2
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 1B
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion1BReponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 3
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 1C
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion1CReponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 4
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 1D
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion1DReponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 5
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 1E
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion1EReponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 6
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 2
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion2Reponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 8
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 3
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion3Reponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 9
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
+                return result.ToList();
+            }
+        }
+
+        //Question 4
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ResponsePOCO> GetQuestion4Reponse()
+        {
+            using (var context = new FSOSSContext())
+            {
+                var result = from x in context.QuestionSelections
+                                where x.question_id == 10
+                                select new ResponsePOCO
+                                {
+                                    Text = x.question_selection_text,
+                                    Value = x.question_selection_value
+                                };
                 return result.ToList();
             }
         }
