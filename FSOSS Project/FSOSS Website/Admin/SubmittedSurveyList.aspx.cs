3c5b368f13dfbdb51e55e8066b34185e907df433
jstewart4 <jstewart39@studentmail.nait.ca>
2018-04-05 18:51:12 -0600

    SubmittedSurveyList - Fixed issue with ListView pager "Invalid postback" error, added commenting

--- FSOSS Project/FSOSS Website/Admin/SubmittedSurveyList.aspx.cs
+++ FSOSS Project/FSOSS Website/Admin/SubmittedSurveyList.aspx.cs
@@ -9,8 +9,15 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_AdministratorPages_SubmittedSurveyList : System.Web.UI.Page
 {
+    // This is for the filter session data that is passed from the previous page, in order to populate the ListView
     public FilterPOCO filter = new FilterPOCO();
 
+    /// <summary>
+    /// When the page loads, the filter session data is brought in from the previous page and it checks to see if the filter is null, if 
+    /// the filter is null then the user is redirected back to the previous page to prevent browsing to this page directly with no data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void Page_Load(object sender, EventArgs e)
     {
         filter = (FilterPOCO)(Session["filter"]);
@@ -20,9 +27,14 @@ public partial class Pages_AdministratorPages_SubmittedSurveyList : System.Web.U
         }
     }
 
-    protected override void OnLoad(EventArgs e)
+    /// <summary>
+    /// This method is to pull the data from filter and populate the ListView. The reason for Page_PreRender is to set the 
+    /// datasource and bind once to avoid error when the page loads.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_PreRender(object sender, EventArgs e)
     {
-        base.OnLoad(e);
         SubmittedSurveyController sysmgr = new SubmittedSurveyController();
         List<SubmittedSurveyPOCO> submittedSurveyData = sysmgr.GetSubmittedSurveyList(filter.siteID, filter.startingDate, filter.endDate, filter.mealID, filter.unitID);
         SubmittedSurveyList.DataSource = submittedSurveyData;
