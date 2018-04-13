e191367f893192cedafcb0e99ccc3bfb17358ade
Rameses Resuena <rdresuena@live.com>
2018-04-13 11:18:35 -0600

    Fixed submit survey bug
    Reason: There was a bug where if a user selects the customer profile checkbox only and clicks submit; the user is not redirected to the Thank You page.

--- FSOSS Project/FSOSS Website/DemographicsPage.aspx.cs
+++ FSOSS Project/FSOSS Website/DemographicsPage.aspx.cs
@@ -123,9 +123,6 @@ public partial class Pages_Survey_DemographicsPage : System.Web.UI.Page
             SubmittedSurveyController sysmgr = new SubmittedSurveyController();
             sysmgr.SubmitSurvey(surveyVersionId, unitId, mealId, participantTypeId, ageRangeId, genderId, contactRequest, contactRoomNumber, contactPhoneNumber, q1AResponse, q1BResponse, q1CResponse, q1DResponse, q1EResponse, q2Response, q3Response, q4Response, q5Response);
 
-            // abandon session
-            Session.Abandon();
-
             Response.Redirect("~/Thankyou");
         }
 
