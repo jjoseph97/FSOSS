0ce12376a972a4c437346ba4291dda8730694f28
Rameses Resuena <rdresuena@live.com>
2018-04-21 22:22:13 -0600

    Commented site master

--- FSOSS Project/FSOSS Website/Site.master.cs
+++ FSOSS Project/FSOSS Website/Site.master.cs
@@ -12,7 +12,11 @@ public partial class SiteMaster : MasterPage
     private const string AntiXsrfTokenKey = "__AntiXsrfToken";
     private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
     private string _antiXsrfTokenValue;
-
+    /// <summary>
+    /// This method was preloaded upon creating the web application. It was not updated by beyond HORIZON SOLUTIONS.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void Page_Init(object sender, EventArgs e)
     {
         // The code below helps to protect against XSRF attacks
@@ -44,7 +48,11 @@ public partial class SiteMaster : MasterPage
 
         Page.PreLoad += master_Page_PreLoad;
     }
-
+    /// <summary>
+    /// This method was preloaded upon creating the web application. It was not updated by beyond HORIZON SOLUTIONS.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void master_Page_PreLoad(object sender, EventArgs e)
     {
         if (!IsPostBack)
@@ -63,68 +71,74 @@ public partial class SiteMaster : MasterPage
             }
         }
     }
-
+    /// <summary>
+    /// This method is used during the web page load.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void Page_Load(object sender, EventArgs e)
     {
+        // Assing the navigation login text based on whether the Administrator is logged in
         LoginButton.Text = Session["adminID"] != null ? "Log out" : "Log in";
-        
+
+        // If the individual is not on the Admin page; hide all Administrator navigation links
         if (!HttpContext.Current.Request.RawUrl.StartsWith("/Admin") && !HttpContext.Current.Request.RawUrl.StartsWith("/admin"))
         {
-            //logolink.HRef = "~/Admin";
-            //LogoLink.PostBackUrl = "~/Admin/";
-            //FSOSSNavbar.Visible = true;
-            //hamburger.Visible = true;
             FSOSSNavbar.Visible = false;
             hamburger.Visible = false;
         }
-        //else
-        //{
-        //    //logolink.HRef = "~/";
-        //    //LogoLink.PostBackUrl = "~/";
-        //    FSOSSNavbar.Visible = false;
-        //    hamburger.Visible = false;
-        //}
 
+        // If the Administrator is logged in
         if (Session["securityID"] != null)
         {
-            if (Session["securityID"].ToString() != "2")
-            {
+            // If the Administrator is not the Master Administrator; hide navigation links for CRUD pages
+            if ((int)Session["securityID"] != 2)
                 MasterAdminDropDown.Visible = false;
-            }
             else
-            {
                 MasterAdminDropDown.Visible = true;
-            }
         }
     }
-
+    /// <summary>
+    /// This method was preloaded upon creating the web application. It was not updated by beyond HORIZON SOLUTIONS.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
     {
         Context.GetOwinContext().Authentication.SignOut();
     }
-
+    /// <summary>
+    /// This method is used when the Administrator clicks the navigation Login button
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void LoginButton_Click(object sender, EventArgs e)
     {
+        // If the button text is "Log out"; clear all session and redirect the Administrator to the Administrator login page
         if (LoginButton.Text == "Log out")
         {
             Session.Abandon();
-            Response.Redirect("~/Admin");
-        }
-        else
-        {
             Response.Redirect("~/Admin/Login.aspx");
         }
+        else
+            Response.Redirect("~/Admin/Login.aspx");
     }
-
+    /// <summary>
+    /// This method is used when the individual clicks the navigation logo
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void LogoLink_Click(object sender, EventArgs e)
     {
+        // If the individual is in the Administrator web page; it will redirect the individual to the Administrator home page
         if (HttpContext.Current.Request.RawUrl.StartsWith("/Admin") || HttpContext.Current.Request.RawUrl.StartsWith("/admin"))
-        {
             Response.Redirect("~/Admin");
-        }
+        // If the individual is taking the survey
         else
         {
+            // Clear all sessions to clear all saved survey answers
             Session.Abandon();
+            // Redirect the individual to the Survey Access page
             Response.Redirect("~/");
         }
     }
