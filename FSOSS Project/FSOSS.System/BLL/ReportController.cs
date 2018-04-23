3072def4d71beaed5252ee67e114ee25402fb6ab
Renzelle Frank Rodrigueza <rrodrigueza1@nait.ca>
2018-04-23 14:14:46 -0600

    Add comments in Generate Report Controller

--- FSOSS Project/FSOSS.System/BLL/ReportController.cs
+++ FSOSS Project/FSOSS.System/BLL/ReportController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 #region
-using System.ComponentModel;
 using FSOSS.System.Data.Entity;
 using FSOSS.System.DAL;
 using FSOSS.System.Data.POCOs;
@@ -13,12 +12,24 @@ namespace FSOSS.System.BLL
 {
     public class ReportController
     {
+        /// <summary>
+        /// This method generate FinalReportPOCO use in chart
+        /// </summary>
+        /// <param name="startDate">Start date of report scope</param>
+        /// <param name="endDate">End date of report scope</param>
+        /// <param name="siteID">Site Id of the report scope</param>
+        /// <param name="mealID">Meal Id of report scope</param>
+        /// <returns></returns>
         public FinalReportPOCO GenerateOverAllReport(DateTime startDate, DateTime endDate, int siteID, int mealID)
         {
+            // Start of Transaction
             using (var context = new FSOSSContext())
             {
+                // Initialize variables
                 List<ParticipantResponse> participantResponseList = new List<ParticipantResponse>();
                 List<SubmittedSurvey> submittedSurveyList = new List<SubmittedSurvey>();
+                // Check to see if the sideId being passes is zero, get all sites if the site id is set to zero
+                // Check to see if the mealId being passes is zero, get all meal if the site id is set to zero 
                 if (siteID == 0 && mealID != 0)
                 {
                     participantResponseList = (from x in context.ParticipantResponses
@@ -67,9 +78,12 @@ namespace FSOSS.System.BLL
                                                                  && x.meal_id == mealID
                                                                  select x).ToList();
                 }
+                // Get the list of question
                 List<Question> questions = (from x in context.Questions
                                             select x).ToList();
+                // Set a list of string to store questions
                 List<string> Questions = new List<string>();
+                // Loop through the loop and retrieve only questions with a response 
                 foreach (Question question in questions)
                 {
                     if (question.question_id == 2)
@@ -107,7 +121,7 @@ namespace FSOSS.System.BLL
                 }
                
 
-              
+                // Initialize all variable required for saving the answer and count
                 List<string> QuestionTwoValueList = new List<string>();
                 List<int> QuestionTwoValueCount = new List<int>();
                 List<string> QuestionThreeValueList = new List<string>();
@@ -124,28 +138,41 @@ namespace FSOSS.System.BLL
                 List<int> QuestionNineValueCount = new List<int>();
                 List<string> QuestionTenValueList = new List<string>();
                 List<int> QuestionTenValueCount = new List<int>();
+                // initialize valueCounter use for counting the answers from the responses
                 int valueCounter = 0;
+                // initialize index use for getting the value from the list
                 int index = 0;
+                // Loop through all the responses from the participant response list
                 foreach (ParticipantResponse responses in participantResponseList)
                 {
+                    // Check to see if the response being check is any of the question with answers                  
                     if (responses.question_id == 2)
                     {
+                        // Check if the answer value contains any answer or the answer is not currently in the list.
+                        // if there are no answer assign the answer and add a +1 on the list
                         if (QuestionTwoValueList.Count < 1 || !QuestionTwoValueList.Contains(responses.participant_answer))
                         {
                             QuestionTwoValueList.Add(responses.participant_answer);
                             valueCounter++;
                             QuestionTwoValueCount.Add(valueCounter);
                         }
+                        // If there is an existing answer on the list
                         else
                         {
+                            // Find the index of the existing value
                             index = QuestionTwoValueList.IndexOf(responses.participant_answer);
+                            // Get the count associated with the existing value
                             valueCounter = QuestionTwoValueCount[index];
+                            // Add a +1 to the value counter
                             valueCounter++;
+                            // replace the old value from the list with the new value from the valueCounter
                             QuestionTwoValueCount[index] = valueCounter;
                         }
+                        // set the value counter to zero
                         valueCounter = 0;
 
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 3)
                     {
                         if (QuestionThreeValueList.Count < 1 || !QuestionThreeValueList.Contains(responses.participant_answer))
@@ -163,6 +190,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 4)
                     {
                         if (QuestionFourValueList.Count < 1 || !QuestionFourValueList.Contains(responses.participant_answer))
@@ -180,6 +208,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 5)
                     {
                         if (QuestionFiveValueList.Count < 1 || !QuestionFiveValueList.Contains(responses.participant_answer))
@@ -197,6 +226,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 6)
                     {
                         if (QuestionSixValueList.Count < 1 || !QuestionSixValueList.Contains(responses.participant_answer))
@@ -214,6 +244,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 8)
                     {
                         if (QuestionEightValueList.Count < 1 || !QuestionEightValueList.Contains(responses.participant_answer))
@@ -231,6 +262,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 9)
                     {
                         if (QuestionNineValueList.Count < 1 || !QuestionNineValueList.Contains(responses.participant_answer))
@@ -248,6 +280,7 @@ namespace FSOSS.System.BLL
                         }
                         valueCounter = 0;
                     }
+                    // Same steps with question_id 2
                     else if (responses.question_id == 10)
                     {
                         if (QuestionTenValueList.Count < 1 || !QuestionTenValueList.Contains(responses.participant_answer))
@@ -266,6 +299,7 @@ namespace FSOSS.System.BLL
                         valueCounter = 0;
                     }
                 }
+                // Initialize finalReport POCO, assign designated list to each properties
                 FinalReportPOCO finalReport = new FinalReportPOCO()
                 {
                     QuestionTwoValueList = QuestionTwoValueList,
@@ -287,125 +321,9 @@ namespace FSOSS.System.BLL
                     SubmittedSurveyList = submittedSurveyList,
                     Question = Questions
                 };
+                // return finalReport POCO 
                 return finalReport;
             }
-        }
-        
-        //public FinalReportPOCO GenerateTrends(DateTime startDate, DateTime endDate, int siteID, int mealID)
-        //{
-        //    using (var context = new FSOSSContext())
-        //    {
-        //        List<ParticipantResponse> participantResponseList = new List<ParticipantResponse>();
-        //        List<SubmittedSurvey> submittedSurveyList = new List<SubmittedSurvey>();
-        //        if (siteID == 0 && mealID != 0)
-        //        {
-        //            participantResponseList = (from x in context.ParticipantResponses
-        //                                       where x.submittedsurvey.date_entered >= startDate
-        //                                       && x.submittedsurvey.date_entered <= endDate
-        //                                       && x.submittedsurvey.meal_id == mealID
-        //                                       select x).ToList();
-        //            submittedSurveyList = (from x in context.SubmittedSurveys
-        //                                   where x.date_entered >= startDate && x.date_entered <= endDate
-        //                                   && x.meal_id == mealID
-        //                                   select x).ToList();
-        //        }
-        //        else if (siteID == 0 && mealID == 0)
-        //        {
-        //            participantResponseList = (from x in context.ParticipantResponses
-        //                                       where x.submittedsurvey.date_entered >= startDate
-        //                                       && x.submittedsurvey.date_entered <= endDate
-        //                                       select x).ToList();
-        //            submittedSurveyList = (from x in context.SubmittedSurveys
-        //                                   where x.date_entered >= startDate && x.date_entered <= endDate
-        //                                   select x).ToList();
-        //        }
-        //        else if (mealID == 0 && siteID != 0)
-        //        {
-        //            participantResponseList = (from x in context.ParticipantResponses
-        //                                       where x.submittedsurvey.date_entered >= startDate
-        //                                       && x.submittedsurvey.date_entered <= endDate
-        //                                       && x.submittedsurvey.Unit.site_id == siteID
-        //                                       select x).ToList();
-        //            submittedSurveyList = (from x in context.SubmittedSurveys
-        //                                   where x.date_entered >= startDate && x.date_entered <= endDate
-        //                                   && x.Unit.site_id == siteID
-        //                                   select x).ToList();
-        //        }
-        //        else
-        //        {
-        //            participantResponseList = (from x in context.ParticipantResponses
-        //                                       where x.submittedsurvey.date_entered >= startDate
-        //                                       && x.submittedsurvey.date_entered <= endDate
-        //                                       && x.submittedsurvey.Unit.site_id == siteID
-        //                                       && x.submittedsurvey.meal_id == mealID
-        //                                       select x).ToList();
-        //            submittedSurveyList = (from x in context.SubmittedSurveys
-        //                                   where x.date_entered >= startDate && x.date_entered <= endDate
-        //                                   && x.Unit.site_id == siteID
-        //                                   && x.meal_id == mealID
-        //                                   select x).ToList();
-        //        }
-        //        List<Question> questions = (from x in context.Questions
-        //                                    select x).ToList();
-        //        List<string> Questions = new List<string>();
-        //        foreach (Question question in questions)
-        //        {
-        //            if (question.question_id == 2)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 3)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 4)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 5)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 6)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 9)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //            else if (question.question_id == 10)
-        //            {
-        //                Questions.Add(question.question_text);
-        //            }
-        //        }
-
-        //        List<string> QuestionTwoValueList = new List<string>();
-        //        List<int> QuestionTwoValueCount = new List<int>();
-        //        List<string> QuestionThreeValueList = new List<string>();
-        //        List<int> QuestionThreeValueCount = new List<int>();
-        //        List<string> QuestionFourValueList = new List<string>();
-        //        List<int> QuestionFourValueCount = new List<int>();
-        //        List<string> QuestionFiveValueList = new List<string>();
-        //        List<int> QuestionFiveValueCount = new List<int>();
-        //        List<string> QuestionSixValueList = new List<string>();
-        //        List<int> QuestionSixValueCount = new List<int>();
-        //        List<string> QuestionNineValueList = new List<string>();
-        //        List<int> QuestionNineValueCount = new List<int>();
-        //        List<string> QuestionTenValueList = new List<string>();
-        //        List<int> QuestionTenValueCount = new List<int>();
-        //        int valueCounter = 0;
-        //        int index = 0;
-
-        //        foreach (ParticipantResponse response in participantResponseList)
-        //        {
-        //            if (response.question_id == 2)
-        //            {
-
-        //            }
-        //        }
-        //    }
-        //}
-        
+        }               
     }
 }
